42eed3bddc2b9265f3fe3b8fe4130895982132b2
Guilherme Henrique <ghpm99@hotmail.com>
2021-08-15 23:28:42 -0300

    Developed get paramet ID

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,11 +1,13 @@
 package one.digitalinnovation.personapi.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import one.digitalinnovation.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
@@ -35,6 +37,14 @@ public class PersonService {
     public List<PersonDTO> listAll() {
         List<Person> allPeople = personRepository.findAll();
         return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
+    }
+
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException{
+        
+        Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+        
+        return personMapper.toDTO(person);
     }
 
 }
